7fc4ba02cff1eacb8868e7d40c003b83de48b811
武蔵野高速交通Y <123287602+MusashinoJM@users.noreply.github.com>
2023-10-22 19:08:59 +0900

    Add files via upload

---
+++ ATOS-Sim/setting.js
@@ -0,0 +1,51 @@
+//「ファイルパスとファイル名」「表示するテキスト」の順で用意した音声パーツの情報を入力してください。
+//予め入力されているサンプルを参考に入力してください。
+//1行1文節で設定してください。
+//無音パーツは流用して使っていただいても構いません。
+//ダブルクォートや括弧、カンマ記号に1文字でも不備があると正しく読み込ませません。
+soundData = 
+[
+ ["sound/k-9ji.wav", "9時・田"],
+ ["sound/k-10.wav", "10・田"],
+ ["sound/k-3min.wav", "3分・田"],
+ ["sound/k-local.wav", "各駅停車・田"],
+ ["sound/k-fuchuhonmachiyukidesu.wav", "府中本町行きです・田"],
+ ["sound/k-hatsu.wav", "発・田"],
+ ["sound/em-thankyoufor.wav", "Thank you for riding with JR East・英・男"],
+ ["sound/em-thenextde.wav", "The next departure from・英・男"],
+ ["sound/em-trackno.wav", "Track No.・英・男"],
+ ["sound/em-tr1.wav", "1(番線)・英・男"],
+ ["sound/em-willbe.wav", "will be・英・男"],
+ ["sound/em-the9.wav", "The9・英・男"],
+ ["sound/em-13min.wav", "13(分)・英・男"],
+  ["sound/em-am.wav", "AM・英・男"],
+ ["sound/em-localtrain.wav", "Local Train・英・男"],
+ ["sound/em-boundfor.wav", "bound for・英・男"],
+ ["sound/em-fuchuhonmachi.wav", "Fuchuhommachi・英・男"],
+ ["sound/k-touekiwodemasuto.wav", "当駅を出ますと・田"],
+ ["sound/k-tsugiha.wav", "次は・田"],
+ ["sound/ka-niiza.wav", "新座・案・田"],
+ ["sound/k-nitomarimasu.wav", "に停まります・田"],
+ ["sound/kt-kitaasaka.wav", "北朝霞・到・田"],
+  ["sound/k-1bansen.wav", "1番線・田"],
+ ["sound/k-drcl.wav", "ドアが閉まります・田"],
+  ["sound/k-gochuikudasai.wav", "御注意下さい・田"],
+ ["sound/s-kinen.wav", "禁煙放送"],
+  ["sound/k-honjitsumo.wav", "本日も・田"],
+  ["sound/k-jrewogoriyou.wav", "JR東日本をご利用くださいまして・田"],
+  ["sound/k-arigatougozaimasu.wav", "ありがとうございます・田"],
+  ["sound/k-kondono.wav", "今度の・田"],
+  ["sound/k-1bansenno.wav", "1番線の・田"],
+  ["sound/k-denshaha.wav", "電車は・田"],
+  ["sound/k-20.wav", "20・田"],
+  ["sound/em-23min.wav", "23(分)・英・男"],
+ 
+ ["sound/null-250.mp3", "(無音0.25秒)"],
+ ["sound/null-500.mp3", "(無音0.5秒)"],
+ ["sound/null-750.mp3", "(無音0.75秒)"],
+ ["sound/null-1000.mp3", "(無音1.0秒)"],
+ ["sound/null-2000.mp3", "(無音2.0秒)"],
+ ["sound/null-3000.mp3", "(無音3.0秒)"],
+
+ ["sound/null-250.mp3", ""] //この最後の行は編集しないでください
+ ]
